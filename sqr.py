47900163ddbb116a2ade21e5f0949aa9611d5627
TypicalProg <52338229+TypicalProg@users.noreply.github.com>
2020-02-27 09:45:25 +0700

    Update sqr.py

--- sqr.py
+++ sqr.py
@@ -31,47 +31,16 @@ def mid(event):
 		x1 = round(x1, 3)
 		x2 = round(x2, 3)
 
-		lbl1['text'] = x1
-		lbl2['text'] = x2
+		lbl1['text'] = 'x1 = ' + str(x1)
+		lbl2['text'] = 'x2 = ' + str(x2)
 
 	elif(d == 0):
 		x1 = -b / (2 * a)
 		x1 = round(x1, 3)
 
-		lbl1['text'] = x1
+		lbl1['text'] = 'x = ' + str(x1)
 		lbl2['text'] = ''
-		
+
 	else:
 		lbl1['text'] = 'Корней нет'
 		lbl2['text'] = ''
-		
-
-
-btn1 = Button(root, text='Вычислить', width=15, font=30)
-
-lbl3 = Label(root, width=20, font=30, text='Введите коэффициенты\nквадратного уравнения:')
-lbl1 = Label(root, width=15, font=30)
-lbl2 = Label(root, width=15, font=30)
-txt_b = Label(root, text='b = ', font=30)
-txt_c = Label(root, text='c = ', font=30)
-txt_a = Label(root, text='a = ', font=30)
-
-ent1 = Entry(root, font=30)
-ent2 = Entry(root, font=30)
-ent3 = Entry(root, font=30)
-
-
-lbl3.grid(row=0, column=0)
-txt_a.grid(row=1, column=0)
-ent1.grid(row=2, column=0)
-txt_b.grid(row=3, column=0)
-ent2.grid(row=4, column=0)
-txt_c.grid(row=5, column=0)
-ent3.grid(row=6, column=0)
-lbl1.grid(row=7, column=0)
-lbl2.grid(row=8, column=0)
-btn1.grid(row=9, column=0)
-
-btn1.bind("<Button-1>", mid)
-
-root.mainloop()
